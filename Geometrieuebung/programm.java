6b93b27eace5a445b84f6777078aa84df14c19dd
Julian Yaman <julianyaman@gmx.de>
2017-10-12 19:48:36 +0200

    Geometrieübung: Enhanced figure drawing, updated README

--- Geometrieuebung/programm.java
+++ Geometrieuebung/programm.java
@@ -32,9 +32,12 @@ public class programm
         
         // drawing triangle
         stift.runter();
-        stift.bewegeBis(140,170);
-        stift.bewegeBis(20,170);
-        stift.bewegeBis(80,80);
+        stift.dreheBis(-60);
+        stift.bewegeUm(100);
+        stift.dreheBis(-180);
+        stift.bewegeUm(100);
+        stift.dreheBis(60);
+        stift.bewegeUm(100);
         
         stift.hoch();
         
@@ -46,28 +49,42 @@ public class programm
         stift.hoch();
         
         // drawing fünfeck (englisch gut)
-        // TODO: finishing fünfeck 
-        stift.bewegeBis(370,80);
-        stift.runter();
-        stift.bewegeBis(415,110);
+        stift.dreheBis(0);
         stift.bewegeBis(400,170);
-        stift.bewegeBis(340,170);
-        stift.bewegeBis(325,110);
-        stift.bewegeBis(370,80);
-        
-        stift.hoch();
-        
-        // drawing pentagram
-        stift.bewegeBis(40,200);
         stift.runter();
-        stift.bewegeBis(140,275);
-        stift.bewegeBis(20,275);
-        stift.bewegeBis(120,200);
-        stift.bewegeBis(80,310);
-        stift.bewegeBis(40,200);
+        stift.dreheUm(72);
+        stift.bewegeUm(60);
+        stift.dreheUm(72);
+        stift.bewegeUm(60);
+        stift.dreheUm(72);
+        stift.bewegeUm(60);
+        stift.dreheUm(72);
+        stift.bewegeUm(60);
+        stift.dreheUm(72);
+        stift.bewegeUm(60);
         
         stift.hoch();
         
+        // drawing pentagramm
+        
+        stift.bewegeBis(80,300);
+        stift.runter();
+        stift.dreheBis(72);
+        stift.bewegeUm(100);
+        stift.dreheBis(216);
+        stift.bewegeUm(100);
+        stift.dreheBis(0);
+        stift.bewegeUm(100);
+        stift.dreheBis(144);
+        stift.bewegeUm(100);
+        stift.dreheBis(-72);
+        stift.bewegeUm(100);
+        
+        
+        stift.hoch();
+
+        stift.dreheBis(0);
+                
         // drawing another thing
         stift.bewegeBis(235,200);
         stift.runter();
