e2d8e0bed5cfc228eaaa024471de8c792e9f9667
Julian Yaman <julianyaman@gmx.de>
2017-10-26 23:58:42 +0200

    Freihandmalerei: Added v7, v8 & v9.0.1

---
+++ Freihandmalerei-Versionen/Freihand7+8+9/app.java
@@ -0,0 +1,75 @@
+import sum.kern.*;
+/**
+ * @author 
+ * @version 
+ */
+public class app
+{
+    // Bezugsobjekte
+    Buntstift derStift;
+    Bildschirm derBildschirm;
+    Maus dieMaus;
+    Tastatur dieTastatur;
+
+    // Konstruktor
+    public app()
+    {
+        derBildschirm = new Bildschirm(500,500);
+        dieMaus = new Maus();
+        derStift = new Buntstift();
+        dieTastatur = new Tastatur();
+    }
+
+    // Dienste
+    public void draw(){
+        do{
+
+            
+            if(dieTastatur.wurdeGedrueckt()){
+                switch(dieTastatur.zeichen()){
+                    case 'r':
+                        derStift.setzeFarbe(Farbe.ROT);
+                        break;
+                    case 'g':
+                        derStift.setzeFarbe(Farbe.GRUEN);
+                        break;
+                    case 'b':
+                        derStift.setzeFarbe(Farbe.BLAU);
+                        break;
+                    case 'y':
+                        derStift.setzeFarbe(Farbe.GELB);
+                        break;
+                    case 'm':
+                        derStift.setzeFarbe(Farbe.MAGENTA);
+                        break;
+                    case 'p':
+                        derStift.setzeFarbe(Farbe.PINK);
+                        break;
+                    case '+':
+                        derStift.setzeLinienBreite(derStift.linienBreite() + 1);
+                        break;
+                    case '-':
+                        derStift.setzeLinienBreite(derStift.linienBreite() - 1);
+                        break;
+                    default:
+                        derStift.setzeFarbe(Farbe.SCHWARZ);
+                }
+                dieTastatur.weiter();
+            }
+            
+            if(dieMaus.istGedrueckt()){
+                derStift.bewegeBis(dieMaus.hPosition(), dieMaus.vPosition());
+                derStift.runter();  
+            }else{
+                derStift.hoch();
+            }
+            
+        }while(!dieMaus.doppelKlick());
+        
+        derStift.gibFrei();
+        derBildschirm.gibFrei();
+        dieMaus.gibFrei();
+        dieTastatur.gibFrei();
+        
+    }
+}
